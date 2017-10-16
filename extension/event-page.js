aed013d3dfaa31442aad6283da84c4516ef0822a
KH <kasperkh.kh@gmail.com>
2017-10-16 20:19:11 +0200

    Imported extension from vidl-web

---
+++ extension/event-page.js
@@ -0,0 +1,60 @@
+var hostURL = "localhost";
+function socketConnect(callback) {
+    ws = new WebSocket("ws://"+hostURL+"/chrome-extension");
+    ws.onopen = function() {
+        if (callback) callback(ws);
+    }
+}
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        startDownload(tabs[0].url, msg.format);
+    });
+});
+chrome.commands.onCommand.addListener(function(command) {
+    if (command == "dl_last") {
+        var lastFormat = localStorage.getItem("lastFormat");
+        if (!lastFormat) lastFormat = "mp3";
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            startDownload(tabs[0].url, lastFormat);
+        });
+    } else if (command == "dl_mp3") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            startDownload(tabs[0].url, "mp3");
+        });
+    } else if (command == "dl_aac") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            startDownload(tabs[0].url, "aac");
+        });
+    } else if (command == "dl_mp4") {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            startDownload(tabs[0].url, "mp4");
+        });
+    }
+});
+function startDownload(url, format) {
+    socketConnect(function(ws) {
+        var message = {
+            url: url,
+            format: format
+        };
+        ws.send(JSON.stringify(message));
+        ws.onmessage = function(e) {
+            var data = JSON.parse(e.data);
+            var type = data.type;
+            if (type == "err") {
+                console.log("err");
+                console.log(data);
+            } else if (type == "info") {
+                console.log("info");
+                console.log(data);
+            } else if (type == "downloaded") {
+                console.log("downloaded");
+                console.log(data);
+            } else if (type == "completed") {
+                console.log("completed");
+                chrome.downloads.download({url:"http://"+hostURL+"/dl/"+data.id});
+            }
+        };
+    });
+}
---
+++ extension/popup.js
@@ -0,0 +1,50 @@
+var url;
+chrome.tabs.query({}, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i].active) {
+            var tab = tabs[i];
+            i = tabs.length;
+            url = tab.url;
+        }
+    }
+});
+
+var lastFormat = localStorage.getItem("lastFormat");
+if (lastFormat) {
+    document.querySelector("#"+lastFormat).checked = true;
+} else {
+    document.querySelector("#mp3").checked = true;
+}
+function updateLastFormat() {
+    var format = "";
+    var inputs = document.querySelectorAll(".options input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].checked) {
+            format = inputs[i].id;
+        }
+    }
+    localStorage.setItem("lastFormat", format);
+    return format;
+}
+document.addEventListener("click", function() {
+    updateLastFormat();
+});
+
+var mp3 = document.querySelector("#mp3");
+mp3.addEventListener("click", function() {
+    init("mp3");
+});
+var aac = document.querySelector("#aac");
+aac.addEventListener("click", function() {
+    init("aac");
+});
+var mp4 = document.querySelector("#mp4");
+mp4.addEventListener("click", function() {
+    init("mp4");
+});
+function init(format) {
+    chrome.runtime.sendMessage({
+        "format": format
+    });
+    window.close();
+}
