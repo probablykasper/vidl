3e451cdff1cea6374c0affa49f40dbe900a2147a
kasp <kasperkh.kh@gmail.com>
2018-04-16 17:12:57 +0200

    Chrome extension icon readded

--- website/src/focus-within-polyfill.js
+++ website/src/focus-within-polyfill.js
@@ -1,31 +1,22 @@
-(function(window, document){
-	'use strict';
-	var slice = [].slice;
-	var removeClass = function(elem){
-		elem.classList.remove('focus-within');
-	};
-	var update = (function(){
-		var running, last;
-		var action = function(){
-			var element = document.activeElement;
-			running = false;
-			if(last !== element){
-				last = element;
-				slice.call(document.getElementsByClassName('focus-within')).forEach(removeClass);
-				while(element && element.classList){
-					element.classList.add('focus-within');
-					element = element.parentNode;
-				}
-			}
-		};
-		return function(){
-			if(!running){
-				requestAnimationFrame(action);
-				running = true;
-			}
-		};
-	})();
-	document.addEventListener('focus', update, true);
-	document.addEventListener('blur', update, true);
-	update();
-})(window, document);
+module.exports = function(className) {
+    if (!className) className = "focus-within";
+    var focusedElements = [];
+    function update() {
+        var focusedElement;
+        while (focusedElement = focusedElements.pop()) {
+            focusedElement.classList.remove(className);
+        }
+
+        // add .focus-within if document has focus,
+        var activeElement = document.activeElement;
+        while (document.hasFocus() && activeElement.nodeName != "#document") {
+            activeElement.classList.add(className);
+            focusedElements.push(activeElement);
+            activeElement = activeElement.parentNode;
+        }
+
+    }
+
+    document.addEventListener("focus", update, true);
+    document.addEventListener("blur", update, true);
+}
--- website/src/global.js
+++ website/src/global.js
@@ -1,3 +1,20 @@
-const VIDL_ENV = "production";
+require("./focus-within-polyfill")("focus-within");
 
-require("./focus-within-polyfill");
+
+
+const isChrome = !!window.chrome && !!window.chrome.webstore;
+if (isChrome) {
+
+    const extLink = "https://chrome.google.com/webstore/detail/ofojemljpdnbfmjenigkncgofkcoacag";
+    const extensionDiv = document.querySelector(".extension.chrome");
+    const svg = extensionDiv.querySelector("svg");
+    svg.addEventListener("click", function() {
+        chrome.webstore.install(extLink, function(suc) {
+            console.log(suc);
+        }, function(err) {
+            console.log(err);
+        });
+    });
+    extensionDiv.classList.add("visible");
+
+}
