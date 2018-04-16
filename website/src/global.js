94179390c3ead066c1a938dc41698b4e729b2b9f
kasp <kasperkh.kh@gmail.com>
2018-04-16 22:20:07 +0200

    Loading spinner

--- website/src/global.js
+++ website/src/global.js
@@ -1,5 +1,33 @@
 require("./focus-within-polyfill")("focus-within");
 
+const middleContainer = document.querySelector(".middle-container");
+window.changeView = function(fromView, toView) {
+    middleContainer.classList.remove(fromView);
+    middleContainer.querySelector(`.${fromView}`).classList.remove("visible");
+    setTimeout(() => {
+        middleContainer.classList.add(toView);
+        middleContainer.querySelector(`.${toView}`).classList.add("visible");
+    }, 280/4);
+}
+window.showTitle = function(title) {
+    middleContainer.classList.add("title-visible");
+    middleContainer.querySelector("p.title").innerHTML = title;
+}
+window.showUploader = function(uploader) {
+    middleContainer.classList.add("uploader-visible");
+    middleContainer.querySelector("p.uploader").innerHTML = uploader;
+}
+window.hideTitle = function(title) {
+    middleContainer.classList.remove("title-visible");
+}
+window.hideUploader = function() {
+    middleContainer.classList.remove("uploader-visible");
+}
+
+setTimeout(() => {
+    changeView("format-selection", "loading-view");
+}, 0);
+
 
 
 const isChrome = !!window.chrome && !!window.chrome.webstore;
