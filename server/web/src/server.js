e6c564ab829ce72387e6aaf3e27a676ec2e6d0bc
KH <kasperkh.kh@gmail.com>
2018-06-15 17:06:40 +0200

    Removed frontend functionality from server

--- server/web/src/server.js
+++ server/web/src/server.js
@@ -3,18 +3,9 @@
 const express = require("express");
 const app = express();
 // local modules
-if (process.env.VIDL_ENV == "dev") require("./modules/compile")();
-require("./modules/init")();
+require("./modules/init");
 
-// load view engine
-app.set("views", "pug");
-app.set("view engine", "pug");
-
-// static content
 const bodyParser = require("body-parser");
-app.use("/", express.static("static", { redirect: false }));
-app.use("/", express.static("static/favicon", { redirect: false }));
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -27,6 +18,3 @@ const server = app.listen(80, () => {
 
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({server});
-
-// routes
-require("./modules/routes")(app, wss);
