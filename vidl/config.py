5c66ae0b4da06eb6f594230c5c629f4c259d5540
Kasper <kasperkh.kh@gmail.com>
2018-10-26 23:47:20 +0200

    Fixed config file not being created

--- vidl/config.py
+++ vidl/config.py
@@ -33,7 +33,7 @@ default_config = {
 }
 
 if not os.path.isfile(config_path):
-    save_file(config_path, default_config)
+    save_file(config_path, default_config, json=True)
 if not os.path.isfile(user_md_parser_path):
     default_user_md_parser = open(default_user_md_parser_path).read()
     save_file(user_md_parser_path, default_user_md_parser)
