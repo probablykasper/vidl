1d2e897fd6ca89f618db71d959e2915162c2ede4
Kasper <kasperkh.kh@gmail.com>
2019-05-01 23:19:25 +0200

    renamed app.py to run.py

--- vidl/dl.py
+++ vidl/dl.py
@@ -3,7 +3,7 @@ import youtube_dl
 from colorboy import cyan, green, red
 from deep_filter import deep_filter
 
-from vidl import log, app, config, md as md_module
+from vidl import log, config, md as md_module
 
 def main():
 
